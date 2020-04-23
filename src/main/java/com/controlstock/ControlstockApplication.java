34e3757d98b752770774bcbd957741a26f0557aa
Tomas Silvestre <tomas.silvestre9@gmail.com>
2020-04-23 20:25:38 -0300

    Creacion del proyecto spring

---
+++ src/main/java/com/controlstock/ControlstockApplication.java
@@ -0,0 +1,13 @@
+package com.controlstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlstock/ControlstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
