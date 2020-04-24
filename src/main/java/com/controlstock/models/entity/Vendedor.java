5e5eb2d59a85bcbd2ea43647801dbacb5fc6274f
Kevin <kevin.nakandakare@gmail.com>
2020-04-23 22:47:14 -0300

    Adding entities.

---
+++ src/main/java/com/controlstock/models/entity/Vendedor.java
@@ -0,0 +1,49 @@
+package com.controlstock.models.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@DiscriminatorValue(value = "Vendedor")
+public class Vendedor extends Empleado {
+	
+	@NotNull
+	private float plus;
+	
+	@NotNull
+	private Sucursal sucursal;
+
+	public Vendedor(String nombre, String apellido, LocalDate fechaNacimiento, long dni, int horasPorJornada,
+			float sueldoBasico, Sucursal sucursal) {
+		super(nombre, apellido, fechaNacimiento, dni, horasPorJornada, sueldoBasico);
+		this.plus = 0;
+		this.sucursal = sucursal;
+	}
+
+	public float getPlus() {
+		return plus;
+	}
+
+	public void setPlus(float plus) {
+		this.plus = plus;
+	}
+
+	public Sucursal getSucursal() {
+		return sucursal;
+	}
+
+	public void setSucursal(Sucursal sucursal) {
+		this.sucursal = sucursal;
+	}
+
+	@Override
+	public String toString() {
+		return "Vendedor [nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento
+				+ ", dni=" + dni + ", plus=" + plus + ", horasPorJornada=" + horasPorJornada + ", sueldoBasico="
+				+ sueldoBasico + "]";
+	}
+}
